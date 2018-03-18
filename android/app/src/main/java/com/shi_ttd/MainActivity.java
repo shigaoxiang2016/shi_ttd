9d35eadcc7765a9e0c15de77f55a85d43fce5dbe
shigaoxiang2016 <752037951@qq.com>
2018-03-19 00:23:48 +0800

    Add files via upload

---
+++ android/app/src/main/java/com/shi_ttd/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shi_ttd;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "shi_ttd";
+    }
+}
